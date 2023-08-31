f76f3f99c2b41aa4c5a5e949e9249007bbc40247
Zoé_Dazy <zoe.dazy+github@gmail.com~>
2023-08-31 17:13:37 +0200

    validation du format fichier uploadé

--- addAddressProcess.php
+++ addAddressProcess.php
@@ -22,12 +22,53 @@ try {
     'phone' => $phone,
     'website' => $website,
     'comment' => $comment,
-    'myFile' => $myFile
 ] = $_POST;
 
-var_dump($_POST); 
+if (isset($_FILES['myFile'])) {
+    // on met le fichier dans une variable pour une meilleure lisibilité
+    $file = $_FILES['myFile'];
+    $filename = $file['name'];
+    $fileType= pathinfo($_FILES['myFile']['name'], PATHINFO_EXTENSION);
+    $allowedFiles = ['jpg', 'png', 'jpeg'];
+        if(in_array($fileType, $allowedFiles)) {
+            echo "type ok";
+        } else {
+            echo "le format du fichier n'est pas compatible";
+        }
+} else {
+    $file = "";
+}
 
-//if name existe pas encore + file est un jpg ou png + zipcode = 5 chiffres + tel
+var_dump($_POST);
+var_dump($file); 
+
+//VALIDATION zipcode = 5 chiffres + tel
+
+
+//TESTS DE CODE DE VALIDATION
+// $allowedFiles = ['jpg', 'png', 'jpeg'];
+// if(in_array($fileType, $allowedFiles)) {
+//     echo "type ok";
+// } else {
+//     echo "le format du fichier n'est pas compatible";
+// }
+
+// if(!empty($_FILES['myFile'])){
+//     //VALIDATION du type de fichier: jpg/png/jpeg.
+// $allowedFiles = ['jpg', 'png', 'jpeg'];
+// //je récupère l'extension et si elle correspond à un élément du tableau $allowedFile, c'est OK.
+// $fileType= pathinfo($_FILES['myFile']['name'], PATHINFO_EXTENSION);
+// var_dump($fileType);
+//     if(in_array($fileType, $allowedFiles)) {
+//         echo "type ok";
+//     } else {
+//         echo "erreur";
+//     }
+// } else {
+//     $myFile = "";
+// }
+
+
 
 // Je double-check (en plus des attributs required insérés dans les inputs du formulaire) que les champs requis ne soient pas vides
 if (!empty($addressName)
